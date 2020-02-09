7b471ba98e08ffe08232d7667021022175f86450
Adam Arling <adam.arling@northwestern.edu>
2020-02-09 14:20:32 -0600

    Update readme again

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,6 +17,7 @@ export default function Footer() {
       </ul>
       <ul className="copyright">
         <li>&copy; Spectral</li>
+        <li>Site by Adam J. Arling</li>
         <li>
           Design: <a href="http://html5up.net">HTML5 UP</a>
         </li>
